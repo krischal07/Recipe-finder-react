3a0a05e1afd6bc80106d4b06ef5f949f4f25afd2
krischal07 <krischal.shrestha9849@gmail.com>
2024-08-18 10:15:26 +0545

    homepage and recipe card created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,34 @@
+import { Heart, HeartPulse, Search, Soup } from "lucide-react";
 import React from "react";
+import RecipeCard from "../components/RecipeCard";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-[#faf9fb] p-10 flex-1 border border-red-600">
+      <div className="max-w-screen-lg mx-auto border border-yellow-600">
+        <form action="">
+          <label htmlFor="" className="input shadow-md flex items-center gap-2">
+            <Search size={24} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow"
+              placeholder="What do you want to cook today?"
+            />
+          </label>
+        </form>
+
+        <p className="font-bold text-3xl md:text-5xl mt-4">
+          Recommended Recipes
+        </p>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular Choices
+        </p>
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 border-4 border-sky-400">
+          <RecipeCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
