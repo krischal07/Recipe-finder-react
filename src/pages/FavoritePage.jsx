a5d1cd301e3e0d09c1de34489971c7ee8dcf96ff
krischal07 <krischal.shrestha9849@gmail.com>
2024-08-18 10:22:54 +0545

    Favorites page created

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import RecipeCard from "../components/RecipeCard";
 
+const fav = true;
 const FavoritePage = () => {
-  return <div>FavoritePage</div>;
+  return (
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="font-bold text-3xl md:test-5xl my-4">My Favorites</p>
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+            <img src="/404.svg" alt="404svg" className="h-3/4" />
+          </div>
+        )}
+
+        {fav && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FavoritePage;
